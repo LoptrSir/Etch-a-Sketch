4009c94fc46baadad5ccb7343870f8e0c245363e
LoptrSir <91218015+LoptrSir@users.noreply.github.com>
2022-08-10 08:59:31 -0700

    start button

--- script1.js
+++ script1.js
@@ -3,10 +3,9 @@
 const container = document.getElementById("container");
 let rows = document.getElementsByClassName("gridRow");
 let cells = document.getElementsByClassName("cell");
-let size = 0;
-//let size = prompt("Input a number up to 100 for the desired grid size.");
+let size = 0; // why not use <let size = :>
 console.log("d", size);
-
+// button
 const button = document.getElementById('btn');
 button.addEventListener('click', function handleClick(event) {
 size = prompt("Input a number up to 100 for the desired grid size.");
@@ -15,32 +14,34 @@ console.log('1', size);
 while (size > 100 || isNaN(size)) {
   alert("Incorrect variable, try again. ");
   size = prompt("Input a number up to 100 for the desired grid size.");
-  console.log('2', size);
-}
-makeGrid();
+  console.log(2,size);
+  }
+makeGrid(); // searches globally for function even if its after this call?
 });
 
 function makeGrid() {
   makeRows(size);
   makeColumns(size);
-  console.log("de");
+  console.log("de", size);
 }
 function makeRows(numRow) {
+  // numRow- getting value from makeRows(size)?
   for (i = 0; i < numRow; i++) {
-    //console.log(i);
+    // why is <i> unasigned element?
     let row = document.createElement("div");
-    //row.className.add("gridRow");
     container.appendChild(row).className = "gridRow";
-    console.log("der");
+    // creating row- just for this iteration to make it append to 'gridRow'?
+    console.log("der", size);
   }
 }
 
 function makeColumns(numCell) {
   for (i = 0; i < rows.length; i++) {
+    // why is the abor for <i> needed?  To make the column nbr match rows?
     for (c = 0; c < numCell; c++) {
       let newCell = document.createElement("div");
       rows[c].appendChild(newCell).className = "cell";
-      console.log("derpy");
+      console.log("derpy", size);
     }
   }
 }

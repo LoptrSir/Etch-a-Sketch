4efe0bd1372d1765793e57761b62c700334e9ab4
LoptrSir <91218015+LoptrSir@users.noreply.github.com>
2022-08-15 11:01:55 -0700

    things to clarify

--- script.js
+++ script.js
@@ -4,32 +4,31 @@ const container = document.getElementById("container");
 let rows = document.getElementsByClassName("gridRow");
 let cells = document.getElementsByClassName("cell");
 let size = 0;
-//let size = prompt("Input a number up to 100 for the desired grid size.");
 console.log("d", size);
 
 const button = document.getElementById("btn");
 button.addEventListener("click", function handleClick(event) {
   size = prompt("Input a number up to 100 for the desired grid size.");
   console.log("1", size);
-
   while (size > 100 || isNaN(size)) {
     alert("Incorrect variable, try again. ");
     size = prompt("Input a number up to 100 for the desired grid size.");
     console.log("2", size);
   }
   makeGrid();
-  console.log("MG");
+  console.log("MG", size);
   const boxes = document.querySelectorAll(".cell");
-boxes.forEach((box) => {
-  box.addEventListener("mouseover", function handleMouseover(event) {
-    console.log("mouseOver", event);
-    if (box.style.background === "green") {
-      boxes.setAttribute("style", "background-color: white;");
-    } else {
-      box.setAttribute("style", "background-color: green;");
-    }
+  //why syntax ',cell' is it always like this?
+  boxes.forEach((box) => {
+    box.addEventListener("mouseover", function handleMouseover(event) {
+      console.log("mouseOver", event);
+      if (box.style.background === "green") {
+        boxes.setAttribute("style", "background-color: white;");
+      } else {
+        box.setAttribute("style", "background-color: green;");
+      }
+    });
   });
-});
 });
 
 function makeGrid() {
@@ -39,16 +38,16 @@ function makeGrid() {
 }
 function makeRows(numRow) {
   for (i = 0; i < numRow; i++) {
-    //console.log(i);
-    let row = document.createElement("div");
-    //row.className.add("gridRow");
-    container.appendChild(row).className = "gridRow";
+       let newRow = document.createElement("div");
+       container.appendChild(newRow).className = "gridRow";
     console.log("der");
   }
 }
 
 function makeColumns(numCell) {
-  for (i = 0; i < rows.length; i++) {
+  for (i = 0; i < rows.length; i++)
+  // is i loop really necessary if c is calling numCell which is the same as numRow?  I assume it has to do with the rows [c].appendChild below.  
+   {
     for (c = 0; c < numCell; c++) {
       let newCell = document.createElement("div");
       rows[c].appendChild(newCell).className = "cell";
@@ -56,8 +55,4 @@ function makeColumns(numCell) {
     }
   }
 }
-//makeGrid();
-
-
-//size = 0;
 console.log("end");
